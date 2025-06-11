df047bed297500dbb94403488222f5d73fafd3b8
Kylian Henry <poutou89@gmail.com>
2025-06-11 12:20:39 +0200

    esquisse

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php ob_start() ?>
+<?php session_start() ?>
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tournoi</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
