88387a44b6348ab4ae177b63f62a27c701808584
Kylian Henry <poutou89@gmail.com>
2025-06-11 13:18:19 +0200

    esquisse

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php ob_start();
-session_start();
-
-$bdd = new PDO('mysql:host=mysql;dbname=Tournois;charset=utf8', 'root', 'root');
+include ('utilities.php')
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
     <?php include('header.php'); ?>
 </body>
-
 </html>
